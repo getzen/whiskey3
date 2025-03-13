3f02e860406f8043f6acc660d8c9f26d8b2b195c
Jim Getzen <jimgetzen@gmail.com>
2025-03-13 13:43:50 -0400

    Update MouseState

--- src/view/mouse_state.rs
+++ src/view/mouse_state.rs
@@ -16,10 +16,13 @@ pub struct MouseState {
     /// The left button was just released and mouse_over is true.
     pub left_button_released: bool,
     /// The mouse position has changed while the left button is down.
-    /// Dragging will remain true while the button is down, even if
-    /// mouse_over becomes false.
+    /// Dragging will remain true while the button remains down, even if
+    /// mouse_over becomes false. This is so a control like a slider will
+    /// work correctly even if the mouse moves off the control.
     pub dragging: bool,
-    pub drag_start_position: Option<Vec2>
+    pub drag_start_position: Option<Vec2>,
+    /// The current position less the drag_start_position.
+    pub drag_delta: Option<Vec2>
 }
 
 impl MouseState {
@@ -34,6 +37,7 @@ impl MouseState {
             left_button_released: false,
             dragging: false,
             drag_start_position: None,
+            drag_delta: None,
         }
     }
 
@@ -72,6 +76,7 @@ impl MouseState {
                 if self.drag_start_position.is_none() {
                     if *point != self.mouse_position {
                         self.drag_start_position = Some(*point);
+                        self.drag_delta = Some(Vec2::ZERO);
                     }
                 } else { 
                     self.dragging = true;
@@ -79,12 +84,12 @@ impl MouseState {
                 }
             }
         } else {
-            // dragging
             if !left_mouse_down {
                 self.dragging = false;
                 //println!("end drag");
                 self.drag_start_position = None;
             } else {
+                self.drag_delta = Some(*point - self.drag_start_position.unwrap());
                 //println!("dragging");
             }
         }
@@ -92,3 +97,24 @@ impl MouseState {
         self.mouse_position = *point;
     }
 }
+
+
+// Sample drag handling in a fn like process_mouse(). It requires the object
+// to have a field called drag_start_pos. It's needed so that the 'grab' point
+// for the object remains the same as its position is updated.
+/*
+let contains_pt = self.contains_point(point, parent_transform);
+self.mouse_state.update(contains_pt, point);
+
+if self.mouse_state.dragging {
+    if self.drag_start_pos.is_none() {
+        // Remember the position of this obj when the drag starts.
+        self.drag_start_pos = Some(self.rect.transform.translation);
+    }
+    let drag_delta = self.mouse_state.drag_delta.unwrap();
+    // Set the current translation as the start position plus the delta.
+    self.rect.transform.translation = self.drag_start_pos.unwrap() + drag_delta;
+} else {
+    self.drag_start_pos = None;
+}
+*/
